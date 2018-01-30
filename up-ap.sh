e3bb7a2c8117e71e267906868c5b9a31d3da1c36
linus <linus@vio.linuslab.org>
2018-01-30 21:56:22 +0800

    foo

--- up-ap.sh
+++ up-ap.sh
@@ -6,10 +6,10 @@ ifconfig wlp0s29u1u3 192.168.0.1
 
 systemctl stop haveged 
 systemctl start haveged 
-
+killall dhcpd
+dhcpd wlp0s29u1u3
 killall hostapd
 hostapd -dd /etc/hostapd/hostapd.conf
 
-killall dhcpd
-dhcpd wlp0s29u1u3
+
 
