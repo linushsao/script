b1b7c3e4028342463e373ce04aef76aef778d506
linus <linus@vio.linuslab.org>
2018-01-30 20:42:19 +0800

    foo

--- up-ap.sh
+++ up-ap.sh
@@ -3,7 +3,12 @@
 #exit 0
 
 ifconfig wlp3s0 192.168.0.1
-systemctl stop hostapd
-systemctl start hostapd
+
+systemctl stop haveged 
+systemctl start haveged 
+
+killall hostapd
+hostapd -dd /etc/hostapd/hostapd.conf
+
 dhcpd wlp3s0
 
