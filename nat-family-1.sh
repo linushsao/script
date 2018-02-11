fbe115bbae07191cdbc3dfbb5ef9a3c2f45fe4d7
linus <linus@vio.linuslab.org>
2018-02-11 18:56:45 +0800

    foo

--- nat-family-1.sh
+++ nat-family-1.sh
@@ -134,6 +134,8 @@ ifconfig $INIF 192.168.0.1
 
 	if  [ "$CREATE_AP" == "TRUE" ]; then
 		#create softAP
+		killall hostapd;sleep 1
+		killall dhcpd;sleep 1
 		create_ap $INIF $EXTIF Linuslab-AP 0726072652
 	else
 		systemctl stop haveged
