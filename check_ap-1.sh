6d0c0832d6db86f7666b1f1c7a222d9f1223cd33
linus <linus@vio.linuslab.org>
2018-02-11 09:53:29 +0800

    foo

--- check_ap-1.sh
+++ check_ap-1.sh
@@ -26,7 +26,7 @@ do
 			c1=`pidof ${PARAM}`
 			echo $c1
 			if [ "$c1" == "" ];then
-			/home/linus/script/nat-family-1.sh --enable-hardreset --enable-reset
+			/home/linus/script/nat-family-1.sh --enable-hardreset
 			echo "TURN ON Ap..."
 			echo "$DTIME :turn on HOSTAPD" >> /home/linus/log/check_ap.log
 			else
