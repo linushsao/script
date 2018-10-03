c2be6de09a16bc550fdaaf6d77474ca390f36ac4
linushsao_in_office <linushsao@gmail.com>
2018-10-03 15:41:23 +0800

    foo

--- es_english.sh
+++ es_english.sh
@@ -2,13 +2,30 @@
 
 #exit 0
 FOLDER="/home/linus/es_english_now"
-
+FOLDER_ALL="/home/linus/es_english"
 case ${1} in
+  "play_all") #PLAY ENGLISH_ALL
+  echo "@@" > /home/linus/log/ES_PLAY
+  until [ `cat /home/linus/log/ES_PLAY` == "" ]
+  do
+	for entry in `ls ${FOLDER_ALL}`; do
+    		#echo $entry
+		mplayer "${FOLDER_ALL}/${entry}"
+	done
+  done
+
+	/home/linus/script/my_log.sh " ES_ENGLISH:play ${entry}..."
+
+	;;
   "play") #PLAY ENGLISH
+  echo "@@" > /home/linus/log/ES_PLAY
+  until [ `cat /home/linus/log/ES_PLAY` == "" ]
+  do
 	for entry in `ls ${FOLDER}`; do
     		#echo $entry
 		mplayer "${FOLDER}/${entry}"
 	done
+  done
 
 	/home/linus/script/my_log.sh " ES_ENGLISH:play ${entry}..."
 
@@ -18,12 +35,11 @@ case ${1} in
 
 	object="mplayer"
 	filter=" "
-
+	echo "" > /home/linus/log/ES_PLAY
 	until [ "${filter}" == "" ]  
   		do
     		filter=`pidof ${object}`
     		kill ${filter} ;sleep 1
-    		echo $filter
 	done
 
 	/home/linus/script/my_log.sh " ES_ENGLISH:terminated..."
